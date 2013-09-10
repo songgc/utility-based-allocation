cccfcdc77f438bef52f3f56930884c15be0635e3
Guocong Song <songgc@gmail.com>
2013-09-09 18:28:26 -0700

    fixed a typo

--- utl_opt.py
+++ utl_opt.py
@@ -17,21 +17,21 @@ def utiliy_opt_2_users(r, u_func, du_func):
     low, high = 0, len(ratio)
     while high - low > 1:
         mid = low + (high - low) / 2
-        _, r_tot = assign_from_midd(r, idx_sort, mid)
+        _, r_tot = assign_from_mid(r, idx_sort, mid)
         if du_func[0](r_tot[0]) / du_func[1](r_tot[1]) < ratio[mid]:
             low, high = low, mid
         else:
             low, high = mid, high
 
-    assign_low, r_tot_low = assign_from_midd(r, idx_sort, low)
-    assign_high, r_tot_high = assign_from_midd(r, idx_sort, high)
+    assign_low, r_tot_low = assign_from_mid(r, idx_sort, low)
+    assign_high, r_tot_high = assign_from_mid(r, idx_sort, high)
     if u_func[0](r_tot_low[0]) + u_func[0](r_tot_low[1]) > u_func[1](r_tot_high[0]) + u_func[1](r_tot_high[1]):
         return assign_low, r_tot_low
     else:
         return assign_high, r_tot_high
 
 
-def assign_from_midd(r, idx_sort, mid):
+def assign_from_mid(r, idx_sort, mid):
     assign = np.array([1] * len(idx_sort))
     assign[idx_sort[0 : mid]] = 0
     r_tot = np.array([r[i, :][assign == i].sum() for i in range(2)])
